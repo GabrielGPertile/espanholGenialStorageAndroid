090d9e9020b5b710a8abb423cde6dc77fdbc09c0
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-10-14 18:50:38 -0300

    Adicionando a página principal ao projeto

---
+++ app/src/main/java/com/example/espanholgenialstorageandroid/activity/LoadingActivity.kt
@@ -0,0 +1,32 @@
+package com.example.espanholgenialstorageandroid.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.espanholgenialstorageandroid.R
+import com.example.espanholgenialstorageandroid.viewHolder.ImageViewHolder
+
+class LoadingActivity : AppCompatActivity()
+{
+    private lateinit var imageViewHolder: ImageViewHolder
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+
+        imageViewHolder = ImageViewHolder(this)
+
+        // Definir um atraso de 3 segundos (3000 milissegundos)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, loginActivity::class.java)
+            startActivity(intent)
+
+            // Fechar a LoadingActivity para que o usuário não consiga voltar para ela
+            finish()
+        },3000)
+
+    }
+}
---
+++ app/src/main/java/com/example/espanholgenialstorageandroid/viewHolder/ImageViewHolder.kt
@@ -0,0 +1,10 @@
+package com.example.espanholgenialstorageandroid.viewHolder
+
+import android.app.Activity
+import android.widget.ImageView
+import com.example.espanholgenialstorageandroid.R
+
+class ImageViewHolder(activity: Activity)
+{
+    val ivImageView: ImageView = activity.findViewById(R.id.ivLogo)
+}
