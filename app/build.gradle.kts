a9e6a900eb9af9126147b9678e959c610560cda8
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-10-14 19:02:38 -0300

    atualizado o compileSDK para o 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.espanholgenialstorageandroid"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.espanholgenialstorageandroid"
