f1831d7e86f44ce21c83b6ce7ea0faf63a3fa945
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-10-19 16:59:54 -0300

    feat: cria ForgetPasswordReset Activity - Inicializa ImageViewHolder e ForgetPasswordResetViewHolder - Configura FirebaseAuth - Prepara layout forget_password_reset

---
+++ app/src/main/java/com/example/espanholgenialstorageandroid/activity/ForgetPasswordReset.kt
@@ -0,0 +1,32 @@
+package com.example.espanholgenialstorageandroid.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.espanholgenialstorageandroid.R
+import com.example.espanholgenialstorageandroid.viewHolder.ForgetPasswordResetViewHolder
+import com.example.espanholgenialstorageandroid.viewHolder.ImageViewHolder
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+
+class ForgetPasswordReset: AppCompatActivity()
+{
+    //declaração dos objetos
+    private lateinit var imageViewHolder: ImageViewHolder
+    private lateinit var forgetPasswordResetViewHolder: ForgetPasswordResetViewHolder
+    private lateinit var auth: FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.forget_password_reset)
+
+        //Inicializa os objetos
+        imageViewHolder = ImageViewHolder(this)
+        forgetPasswordResetViewHolder = ForgetPasswordResetViewHolder(this)
+
+        //Inicializa o Auth do Firebase
+        FirebaseApp.initializeApp(this)
+        auth = FirebaseAuth.getInstance()
+    }
+}
