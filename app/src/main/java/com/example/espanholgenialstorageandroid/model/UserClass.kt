4009418b39c8680cbf77cdb99a5f42992a06b5e5
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-10-20 20:22:02 -0300

    feat: adicionando a classe dos usuário no sistema

---
+++ app/src/main/java/com/example/espanholgenialstorageandroid/model/UserClass.kt
@@ -0,0 +1,10 @@
+package com.example.espanholgenialstorageandroid.model
+
+data class UserClass (
+    var uid: String = "",
+    var nomeCompleto: String = "",
+    var fotoPerfil: String = "",
+    var tipoUsuario: String = "",
+    var idade: Int = 0,
+    var email: String = "",
+)
