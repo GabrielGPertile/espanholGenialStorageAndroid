dd307ab62b809e983579362713fbc28f40d9b16f
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-10-22 19:54:17 -0300

    feat: adicionando o view holder de activity do usuario

---
+++ app/src/main/java/com/example/espanholgenialstorageandroid/viewHolder/UserActivityViewHolder.kt
@@ -0,0 +1,28 @@
+package com.example.espanholgenialstorageandroid.viewHolder
+
+import android.app.Activity
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.espanholgenialstorageandroid.R
+import com.google.android.material.navigation.NavigationView
+
+class UserActivityViewHolder(activity: Activity)
+{
+    //layout principal
+    val ivPerfilUsuario: ImageView = activity.findViewById(R.id.ivPerfilUsuario)
+    val tvNomeCompleto: TextView = activity.findViewById(R.id.tvNomeCompleto)
+    val tvNomeCompletoDado: TextView = activity.findViewById(R.id.tvNomeCompletoDado)
+    val tvEmail: TextView = activity.findViewById(R.id.tvEmail)
+    val tvEmailDado: TextView = activity.findViewById(R.id.tvEmailDado)
+    val tvIdade: TextView = activity.findViewById(R.id.tvIdade)
+    val tvIdadeDado: TextView = activity.findViewById(R.id.tvIdadeDado)
+    val tvTipoUsuario: TextView = activity.findViewById(R.id.tvTipoUsuario)
+    val tvTipoUsuarioDado: TextView = activity.findViewById(R.id.tvTipoUsuarioDado)
+
+    // Elementos do menu lateral
+    var toolbar: Toolbar =  activity.findViewById(R.id.toolbar)
+    val drawerLayout: DrawerLayout = activity.findViewById(R.id.drawer_layout)
+    val navView: NavigationView = activity.findViewById(R.id.nav_view)
+}
