62ac078a2f46b3ab1e0c4ba83797bbb61b1afb09
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-10-22 19:55:25 -0300

    feat: adicionando o deslog do menu lateral na activity do user

--- app/src/main/java/com/example/espanholgenialstorageandroid/activity/UserActivity.kt
+++ app/src/main/java/com/example/espanholgenialstorageandroid/activity/UserActivity.kt
@@ -1,11 +1,13 @@
 package com.example.espanholgenialstorageandroid.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.espanholgenialstorageandroid.R
 import com.example.espanholgenialstorageandroid.viewHolder.UserActivityViewHolder
+import com.google.firebase.auth.FirebaseAuth
 
 class UserActivity: AppCompatActivity()
 {
@@ -44,5 +46,16 @@ class UserActivity: AppCompatActivity()
             userActivityViewHolder.drawerLayout.closeDrawers()
             true
         }
+    }
+
+    private fun deslogFirebase()
+    {
+        // desloga do Firebase
+        FirebaseAuth.getInstance().signOut()
+        Toast.makeText(this, "Deslogado com sucesso!", Toast.LENGTH_SHORT).show()
+
+        val intent = Intent(this, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
     }
 }
