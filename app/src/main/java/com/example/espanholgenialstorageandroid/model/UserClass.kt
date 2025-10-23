8ac468b777726d05e1710363adfc35958c046686
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-10-23 19:39:10 -0300

    fix: removendo o tipoUsuario

--- app/src/main/java/com/example/espanholgenialstorageandroid/model/UserClass.kt
+++ app/src/main/java/com/example/espanholgenialstorageandroid/model/UserClass.kt
@@ -4,7 +4,6 @@ data class UserClass (
     var uid: String = "",
     var nomeCompleto: String = "",
     var fotoPerfil: String = "",
-    var tipoUsuario: String = "",
     var idade: Int = 0,
     var email: String = "",
 )
