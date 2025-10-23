91e641187a91ef38600f747a46a94c0bee1d73f2
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-10-23 19:38:49 -0300

    feat: adicionando o layout para a edição do usuário e o seu viewholder

---
+++ app/src/main/java/com/example/espanholgenialstorageandroid/viewHolder/UserPerfileEditableViewHolder.kt
@@ -0,0 +1,30 @@
+package com.example.espanholgenialstorageandroid.viewHolder
+
+import android.app.Activity
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.espanholgenialstorageandroid.R
+import com.google.android.material.navigation.NavigationView
+
+class UserPerfileEditableViewHolder(activity: Activity)
+{
+    //layout principal
+    val ivPerfilUsuario: ImageView = activity.findViewById(R.id.ivPerfilUsuario)
+    val tvNomeCompleto: TextView = activity.findViewById(R.id.tvNomeCompleto)
+    val etNomeCompletoDado: EditText = activity.findViewById(R.id.etNomeCompletoDado)
+    val tvEmail: TextView = activity.findViewById(R.id.tvEmail)
+    val etEmailDado: EditText = activity.findViewById(R.id.etEmailDado)
+    val tvIdade: TextView = activity.findViewById(R.id.tvIdade)
+    val etIdadeDado: EditText = activity.findViewById(R.id.etIdadeDado)
+    val btnSalvar: Button = activity.findViewById(R.id.btnSalvar)
+    val btnCanelar: Button = activity.findViewById(R.id.btnCanelar)
+
+    // Elementos do menu lateral
+    var toolbar: Toolbar =  activity.findViewById(R.id.toolbar)
+    val drawerLayout: DrawerLayout = activity.findViewById(R.id.drawer_layout)
+    val navView: NavigationView = activity.findViewById(R.id.nav_view)
+}
