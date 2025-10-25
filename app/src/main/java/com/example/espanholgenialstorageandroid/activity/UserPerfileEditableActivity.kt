a3251ccbce779736369208db24e2f571582fd07d
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-10-25 16:24:05 -0300

    feat: adicionando a configuração para a edição de imagem

--- app/src/main/java/com/example/espanholgenialstorageandroid/activity/UserPerfileEditableActivity.kt
+++ app/src/main/java/com/example/espanholgenialstorageandroid/activity/UserPerfileEditableActivity.kt
@@ -4,13 +4,14 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.activity.result.ActivityResultLauncher
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.espanholgenialstorageandroid.R
 import com.example.espanholgenialstorageandroid.viewHolder.UserPerfileEditableViewHolder
+import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.storage.FirebaseStorage
 
-class UserPerfileEditableActivity: AppCompatActivity()
+class UserPerfileEditableActivity: BaseDrawerActivity()
 {
     private lateinit var userPerfileEditableViewHolder: UserPerfileEditableViewHolder
     private lateinit var pickImageLauncher: ActivityResultLauncher<Intent>
@@ -22,5 +23,46 @@ class UserPerfileEditableActivity: AppCompatActivity()
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.user_perfile_editable_activity)
+
+        //Inicializa os objetos
+        userPerfileEditableViewHolder = UserPerfileEditableViewHolder(this)
+
+        //Inicializa o Auth do Firebase
+        FirebaseApp.initializeApp(this)
+        auth = FirebaseAuth.getInstance()
+        storage = FirebaseStorage.getInstance()
+
+        //configura o menu lateral
+        setupDrawer(
+            userPerfileEditableViewHolder.drawerLayout,
+            userPerfileEditableViewHolder.navView,
+            userPerfileEditableViewHolder.toolbar
+        )
+
+        pickImageLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if(result.resultCode == RESULT_OK)
+            {
+                val data: Intent? = result.data
+                val uri = data?.data
+
+                if(uri != null)
+                {
+                    selectedImageUri = uri
+                    userPerfileEditableViewHolder.ivPerfilUsuario.setImageURI(uri)
+                    savedEditablePhoto(uri)
+                }
+            }
+        }
+
+        //configuração da edição da imagem
+        userPerfileEditableViewHolder.ivPerfilUsuario.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK).apply {
+                type = "image/*"
+            }
+
+            pickImageLauncher.launch(intent)
+        }
     }
 }
