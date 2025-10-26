4ad89463f28cca235e42fd01be962401f0d15271
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-10-26 20:49:22 -0300

    feat: implementando a função que navega entre a tela do dashboard para a tela de criação de imagens no storage

--- app/src/main/java/com/example/espanholgenialstorageandroid/activity/DashboardActivity.kt
+++ app/src/main/java/com/example/espanholgenialstorageandroid/activity/DashboardActivity.kt
@@ -1,5 +1,6 @@
 package com.example.espanholgenialstorageandroid.activity
 
+import android.content.Intent
 import android.os.Bundle
 import com.example.espanholgenialstorageandroid.R
 import com.example.espanholgenialstorageandroid.viewHolder.DashboardActivityViewHolder
@@ -20,5 +21,17 @@ class DashboardActivity: BaseDrawerActivity()
         )
 
         loadProfilePhotoInDrawer()
+
+        //configurações dos botões
+        dashboardActivityViewHolder.ivInserirImagem.setOnClickListener {
+            navigateToCreateImage()
+        }
+    }
+
+    private fun navigateToCreateImage()
+    {
+        val intent = Intent(this, CreatePhotoStorageActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
     }
 }
