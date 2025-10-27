ff0a46de0a16adcf0642dc230d7437a6023d4a48
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-10-26 21:06:24 -0300

    feat: adicionando o firebase para a tela de criar imagens no storage

--- app/src/main/java/com/example/espanholgenialstorageandroid/activity/CreatePhotoStorageActivity.kt
+++ app/src/main/java/com/example/espanholgenialstorageandroid/activity/CreatePhotoStorageActivity.kt
@@ -6,13 +6,20 @@ import android.os.Bundle
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.espanholgenialstorageandroid.R
+import com.example.espanholgenialstorageandroid.model.UserClass
 import com.example.espanholgenialstorageandroid.viewHolder.CreatePhotoStorageViewHolder
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
 
 class CreatePhotoStorageActivity : BaseDrawerActivity()
 {
     private lateinit var createPhotoStorageViewHolder: CreatePhotoStorageViewHolder
     private lateinit var pickImageLauncher: ActivityResultLauncher<Intent>
     private var selectedImageUri: Uri? = null
+    private lateinit var auth: FirebaseAuth
+    private lateinit var storage: FirebaseStorage
+    private lateinit var user: UserClass
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
@@ -20,6 +27,11 @@ class CreatePhotoStorageActivity : BaseDrawerActivity()
         setContentView(R.layout.create_photo_storage)
 
         createPhotoStorageViewHolder = CreatePhotoStorageViewHolder(this)
+
+        //Inicializa o Auth do Firebase
+        FirebaseApp.initializeApp(this)
+        auth = FirebaseAuth.getInstance()
+        storage = FirebaseStorage.getInstance()
 
         setupDrawer(
             createPhotoStorageViewHolder.drawerLayout,
