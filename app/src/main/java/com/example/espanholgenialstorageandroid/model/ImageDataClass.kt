08c8f156db6cf1e99847ee7b815eda97df0b4c57
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-10-28 19:01:46 -0300

    feat: adicionando a classe que criar imagens no firebase storage

---
+++ app/src/main/java/com/example/espanholgenialstorageandroid/model/ImageDataClass.kt
@@ -0,0 +1,8 @@
+package com.example.espanholgenialstorageandroid.model
+
+data class ImageDataClass (
+    val nomePt: String = "",
+    val nomeEs: String = "",
+    val url: String = "",
+    val userId: String = ""
+)
