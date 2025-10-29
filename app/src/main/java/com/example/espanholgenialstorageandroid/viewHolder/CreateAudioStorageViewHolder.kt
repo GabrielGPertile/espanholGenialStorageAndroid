5e38dc3821cff2cbdb07ae8316abc556538e0232
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-10-29 19:08:20 -0300

    feat: Criar DashboardActivityViewHolder

---
+++ app/src/main/java/com/example/espanholgenialstorageandroid/viewHolder/CreateAudioStorageViewHolder.kt
@@ -0,0 +1,26 @@
+package com.example.espanholgenialstorageandroid.viewHolder
+
+import android.app.Activity
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.espanholgenialstorageandroid.R
+import com.google.android.material.navigation.NavigationView
+
+class CreateAudioStorageViewHolder(activity: Activity)
+{
+    val ivAudio: ImageView = activity.findViewById(R.id.ivAudio)
+    val tvAudioName: TextView = activity.findViewById(R.id.tvAudioName)
+    val etAudioName: EditText = activity.findViewById(R.id.etAudioName)
+    val btnCasoDeUso: Button = activity.findViewById(R.id.btnCasoDeUso)
+    val btnSalvar: Button = activity.findViewById(R.id.btnSalvar)
+    val btnCanelar: Button = activity.findViewById(R.id.btnCancelar)
+
+    // Elementos do menu lateral
+    var toolbar: Toolbar =  activity.findViewById(R.id.toolbar)
+    val drawerLayout: DrawerLayout = activity.findViewById(R.id.drawer_layout)
+    val navView: NavigationView = activity.findViewById(R.id.nav_view)
+}
