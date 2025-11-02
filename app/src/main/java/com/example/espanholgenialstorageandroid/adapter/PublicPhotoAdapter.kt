3ee0fa547bd7b7536479f4602e3123ea04431e14
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-11-02 08:25:39 -0300

    feat: Adicionando os 3 botões que represetam as 3 funcionaliades básicas que são visualizar, excluir e tornar público.

--- app/src/main/java/com/example/espanholgenialstorageandroid/adapter/PublicPhotoAdapter.kt
+++ app/src/main/java/com/example/espanholgenialstorageandroid/adapter/PublicPhotoAdapter.kt
@@ -3,16 +3,23 @@ package com.example.espanholgenialstorageandroid.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.espanholgenialstorageandroid.R
 
 class PublicPhotoAdapter(
     private val listaImagensPublicas: MutableList<String>,
+    private val onVisualizar: (String) -> Unit,
+    private val onExcluir: (String) -> Unit,
+    private val onTornarPublico: (String) -> Unit,
 ): RecyclerView.Adapter<PublicPhotoAdapter.ImagemViewHolder>()
 {
     class ImagemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val textViewNome: TextView = itemView.findViewById(R.id.textViewNomeImagem)
+        val btnVisualizar: ImageButton = itemView.findViewById(R.id.btnVisualizar)
+        val btnExcluir: ImageButton = itemView.findViewById(R.id.btnExcluir)
+        val btnTornarPublico: ImageButton = itemView.findViewById(R.id.btnTornarPrivado)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImagemViewHolder {
@@ -24,6 +31,10 @@ class PublicPhotoAdapter(
     override fun onBindViewHolder(holder: ImagemViewHolder, position: Int) {
         val nome = listaImagensPublicas[position]
         holder.textViewNome.text = nome
+
+        holder.btnVisualizar.setOnClickListener { onVisualizar(nome) }
+        holder.btnExcluir.setOnClickListener { onExcluir(nome) }
+        holder.btnTornarPublico.setOnClickListener { onTornarPublico(nome) }
     }
 
     override fun getItemCount(): Int = listaImagensPublicas.size
