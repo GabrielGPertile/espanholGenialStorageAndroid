b245cb0088aba213d822f7b241609717049ddc57
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-11-02 13:43:16 -0300

    feat: inicializando as variaveis

--- app/src/main/java/com/example/espanholgenialstorageandroid/activity/ListarAudioPublicosAcitivity.kt
+++ app/src/main/java/com/example/espanholgenialstorageandroid/activity/ListarAudioPublicosAcitivity.kt
@@ -1,11 +1,17 @@
 package com.example.espanholgenialstorageandroid.activity
 
 import android.net.Uri
+import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.espanholgenialstorageandroid.adapter.PrivateAudioAdapter
+import com.example.espanholgenialstorageandroid.R
+import com.example.espanholgenialstorageandroid.adapter.PublicAudioAdapter
 import com.example.espanholgenialstorageandroid.strategy.SanitizeFileNameInterface
 import com.example.espanholgenialstorageandroid.strategy.SanitizeFileNameStrategy
+import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
@@ -13,7 +19,7 @@ import com.google.firebase.storage.FirebaseStorage
 class ListarAudioPublicosAcitivity: BaseDrawerActivity()
 {
     private lateinit var recyclerView: RecyclerView
-    private lateinit var adapter: PrivateAudioAdapter
+    private lateinit var adapter: PublicAudioAdapter
     private val listaAudios = mutableListOf<String>()
     private lateinit var selecionarAudioLauncher: ActivityResultLauncher<String>
     private var audioSelecionadaUri: Uri? = null
@@ -21,4 +27,50 @@ class ListarAudioPublicosAcitivity: BaseDrawerActivity()
     private lateinit var storage: FirebaseStorage
     private lateinit var firestore: FirebaseFirestore
     private val sanitizer: SanitizeFileNameInterface = SanitizeFileNameStrategy()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.listar_audios_privados)
+
+        // Configura o launcher
+        selecionarAudioLauncher = registerForActivityResult(
+            ActivityResultContracts.GetContent()
+        ) { uri ->
+            if (uri != null) {
+                audioSelecionadaUri = uri
+                Toast.makeText(this, "Imagem selecionada!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        // 🔹 Inicializa as views do Drawer e Toolbar
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        val drawerLayout = findViewById<androidx.drawerlayout.widget.DrawerLayout>(R.id.drawer_layout)
+        val navView = findViewById<com.google.android.material.navigation.NavigationView>(R.id.nav_view)
+
+        //Inicializa o Auth do Firebase
+        FirebaseApp.initializeApp(this)
+        auth = FirebaseAuth.getInstance()
+        storage = FirebaseStorage.getInstance()
+        firestore = FirebaseFirestore.getInstance()
+
+        setupDrawer(
+            drawerLayout,
+            navView,
+            toolbar
+        )
+
+        loadProfilePhotoInDrawer()
+
+        recyclerView = findViewById(R.id.recyclerViewAudios)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = PublicAudioAdapter(
+            listaAudios,
+            onVisualizar = { nome -> visualizarAudio(nome) },
+            onExcluir = { nome -> excluirAudio(nome) },
+            onTornarPublico = { nome -> tornarAudioPublico(nome) { carregarNomesAudios()} }
+        )
+        recyclerView.adapter = adapter
+
+        carregarNomesAudios()
+    }
 }
