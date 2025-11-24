644982139f5a4484d39cffd1278db1a34b6de31e
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-11-24 20:26:36 -0300

    chore: firebase e firestore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.firebase.firestore.ktx)
+    implementation(libs.firebase.database)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
